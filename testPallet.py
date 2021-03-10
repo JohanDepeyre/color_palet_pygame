5661e3fd2afe66db73c7c56486d4d052862e2b05
JohanDepeyre <43540917+JohanDepeyre@users.noreply.github.com>
2021-03-10 10:31:16 +0100

    Update testPallet.py

--- testPallet.py
+++ testPallet.py
@@ -6,27 +6,27 @@ pygame.init()
 fenetre = pygame.display.set_mode((1200, 600))
 
 
-
-
-
-# Chargement et collage du fond
-fond = pygame.image.load("test.png").convert()
+#add image
+fond = pygame.image.load("palet.png").convert()
+#show image
 fenetre.blit(fond, (0, 0))
 
 while True:
     # Rafraichissement
 
 
-    for event in pygame.event.get():  # Attente des événements
+    for event in pygame.event.get():  # wait event
+    #click event
         if event.type == MOUSEBUTTONDOWN:
             coordinates = pygame.mouse.get_pos()
 
             # mouse_position is in the form [x,y], we only want the x part
             x = coordinates[0]
             y = coordinates[1]
-
-            print(fond.get_at((x, y)))
-            pygame.draw.line(fenetre, fond.get_at((x, y)) , (5,500), (500,500), 50)
+            #get RGBA color in x,y position
+            color = fond.get_at((x, y))
+            #draw line with de color selected
+            pygame.draw.line(fenetre, color , (5,500), (500,500), 50)
                 
   
   
